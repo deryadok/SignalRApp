1de261c69ea61a7dcd3afa3b63ccf2e5520af040
Derya Dok <dokderya@gmail.com>
2023-07-24 17:13:33 +0300

    Add project files.

---
+++ SignalRApp.API/Hubs/MyHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRApp.API.Hubs
+{
+    public class MyHub : Hub
+    {
+        public static List<string> Messages { get; set; } = new List<string>();
+
+        public async Task SendMessage(string message)
+        {
+            Messages.Add(message);
+            await Clients.All.SendAsync("RecieveMessage", message);
+        }
+
+        public async Task GetMessage()
+        {
+            await Clients.All.SendAsync("RecieveMessages", Messages);
+        }
+    }
+}
---
+++ SignalRApp.API/Program.cs
@@ -0,0 +1,37 @@
+using SignalRApp.API.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddSignalR();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", builder =>
+    {
+        builder.WithOrigins("https://localhost:7035").AllowAnyHeader().AllowAnyMethod().AllowCredentials();
+    });
+});
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("CorsPolicy");
+
+app.UseAuthorization();
+
+app.MapControllers();
+app.MapHub<MyHub>("/MyHub");
+
+app.Run();
