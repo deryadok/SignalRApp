75926a05b12924c6c9ac2a49aee4475f73f051de
Derya Dok <dokderya@gmail.com>
2023-08-08 17:10:29 +0300

    Grouping

--- SignalRApp.API/Models/AppDbContext.cs
+++ SignalRApp.API/Models/AppDbContext.cs
@@ -1,6 +1,12 @@
-﻿ namespace SignalRApp.API.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SignalRApp.API.Models
 {
-    public class AppDbContext 
+    public class AppDbContext :DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
     }
 }
