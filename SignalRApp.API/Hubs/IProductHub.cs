0fd5603cf9e23a4a42e5590682b781bc7536e61d
deryadok <dokderya@gmail.com>
2023-08-10 18:31:44 +0300

    Strongly typed hub added

---
+++ SignalRApp.API/Hubs/IProductHub.cs
@@ -0,0 +1,9 @@
+﻿using SignalRApp.API.Models;
+
+namespace SignalRApp.API.Hubs
+{
+    public interface IProductHub
+    {
+        Task RecieveProduct(Product p);
+    }
+}
---
+++ SignalRApp.API/Hubs/ProductHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SignalRApp.API.Models;
+
+namespace SignalRApp.API.Hubs
+{
+    public class ProductHub : Hub<IProductHub>
+    {
+        public async Task SendProduct(Product p)
+        {
+            await Clients.All.RecieveProduct(p);
+        }
+    }
+}
